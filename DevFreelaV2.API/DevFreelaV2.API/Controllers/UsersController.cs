1edfc74c127284ceb3afce2c941517d9306caac6
Victor Mello <victorsmello93@gmail.com>
2021-02-13 20:33:54 -0300

    Criados os endpoints da API de usuários e projeto

---
+++ DevFreelaV2.API/DevFreelaV2.API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DevFreelaV2.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreelaV2.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        //Ex: api/users/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+        }
+
+        //Ex: api/users/1/login
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, [FromBody] LoginModel loginModel)
+        {
+            return NoContent();
+
+            //Será refatorado para implementar a lógica para retornar uma senha encriptada através da chave de autenticação gerada do token JWT
+        }
+    }
+}
---
+++ DevFreelaV2.API/DevFreelaV2.API/Models/UpdateProjectModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreelaV2.API.Models
+{
+    public class UpdateProjectModel
+    {
+        public string Description { get; set; }
+    }
+}
